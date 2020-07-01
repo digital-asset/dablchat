429bc76fdac10880d793c0f9a3e1f76bbf4db939
Sarah Breckenridge <sarah.breckenridge@digitalasset.com>
2020-07-01 10:21:45 -0400

    version updates

--- python/operator/setup.py
+++ python/operator/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dablchat-operator-bot',
-      version='1.7.2',
+      version='1.8.0',
       description='DABL Chat Operator',
       author='Digital Asset',
       license='Apache2',
--- python/user/setup.py
+++ python/user/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dablchat-user-bot',
-      version='1.7.2',
+      version='1.8.0',
       description='DABL Chat User',
       author='Digital Asset',
       license='Apache2',
